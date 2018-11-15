9f248b2913038cda072edf1e031533ef7e7695f6
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2018-11-14 21:58:14 -0600

    pop

--- theme-config.sh
+++ theme-config.sh
@@ -1,16 +1,16 @@
-sudo add-apt-repository ppa:noobslab/icons2
-sudo apt-get update
-sudo apt-get install uniform-icons
+#sudo add-apt-repository ppa:noobslab/icons2
+#sudo apt-get update
+#sudo apt-get install uniform-icons
 
-sudo add-apt-repository ppa:noobslab/icons
-sudo apt-get update
-sudo apt-get install dalisha-icons
+#sudo add-apt-repository ppa:noobslab/icons
+#sudo apt-get update
+#sudo apt-get install dalisha-icons
 
 ######## pop icons
 sudo add-apt-repository ppa:system76/pop
 sudo apt update
 sudo apt install pop-icon-theme
 
-sudo add-apt-repository ppa:snwh/ppa
-sudo apt-get update
-sudo apt-get install paper-icon-theme
+#sudo add-apt-repository ppa:snwh/ppa
+#sudo apt-get update
+#sudo apt-get install paper-icon-theme
