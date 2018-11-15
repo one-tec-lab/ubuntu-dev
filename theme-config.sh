1906f9c4b09cd97d38c28da5bbc3edfcf1d7e63b
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2018-11-14 21:11:42 -0600

    create theme config

---
+++ theme-config.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:noobslab/icons2
+sudo apt-get update
+sudo apt-get install uniform-icons
+
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install dalisha-icons
