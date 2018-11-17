c40552543af6a032b065ba21892c7cbf940808d4
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2018-11-16 19:21:05 -0600

    fixed EOF

--- install-dev.sh
+++ install-dev.sh
@@ -187,13 +187,13 @@ function install-desktop {
    echo
 
    sudo bash -c "cat >/etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla" <<EOF
-   [Allow Colord all Users]
-   Identity=unix-user:*
-   Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
-   ResultAny=no
-   ResultInactive=no
-   ResultActive=yes
-   EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
 
    #---------------------------------------------------#
    # Step 5 - Enable Extensions ....
