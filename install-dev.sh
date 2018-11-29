562c93c1fbc5155b8bb541d1fc8ddc03256202bb
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2018-11-28 20:26:29 -0600

    update compse

--- install-dev.sh
+++ install-dev.sh
@@ -104,7 +104,7 @@ function install-server {
    sudo apt-get update
    sudo apt-get install docker-ce -y
 
-   sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+   sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
    sudo chmod +x /usr/local/bin/docker-compose
 
    sudo groupadd docker
